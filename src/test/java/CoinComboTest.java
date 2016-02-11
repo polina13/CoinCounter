6ab5c8d268faa5cd5d045c49b360b19e948fdd7e
Polina Nenchev and Ryan King <polina.mihova@yahoo.com>
2016-02-11 09:02:35 -0800

    Create first passing test for quarters

--- src/test/java/CoinComboTest.java
+++ src/test/java/CoinComboTest.java
@@ -5,14 +5,14 @@ import java.util.ArrayList;
 public class CoinComboTest {
 
   @Test
-  public void makesChange() {
+  public void makesChange_testForQuarters() {
     CoinCombo testCoinCombo = new CoinCombo();
     assertEquals("Your change for 27 cents is: 1 quarters", testCoinCombo.makesChange(27));
   }
 
   @Test
-  public void makesChange() {
+  public void makesChange_testForDimes() {
     CoinCombo testCoinCombo = new CoinCombo();
-    assertEquals("Your change for 27 cents is: 1 quarters", testCoinCombo.makesChange(27));
+    assertEquals("Your change for 37 cents is: 1 quarters, 1 dimes", testCoinCombo.makesChange(37));
   }
 }
